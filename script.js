e9d324695ec975c06254a295f17b1eaac5dfbb69
Sára Kraibich Tesařová <sara.k.tesarova@gmail.com>
2024-04-19 12:21:37 +0200

    Úkol 01 - Fahrenheit vs. Celsius

---
+++ script.js
@@ -0,0 +1,6 @@
+// Fahrenheit vs. Celsius
+
+const stupneFahrenheita = Number(prompt('Zadejte teplotu ve stupních Fahrenheita:'))
+
+document.body.innerHTML = '<h1>' + 'Převodník stupňů Fahrenheita na stupně Celsia' + '</h1>'
+document.body.innerHTML += '<p>' + 'Teplota ve stupních Celsia: ' + 5 * (stupneFahrenheita - 32) / 9 + ' F ' + '</p>'
