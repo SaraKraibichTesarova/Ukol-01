f23ba657cb5a86ec6ab6f96e2bdbe3f6089c2599
Sára Kraibich Tesařová <sara.k.tesarova@gmail.com>
2024-04-19 12:35:08 +0200

    Úkol 01

--- script.js
+++ script.js
@@ -3,4 +3,4 @@
 const stupneFahrenheita = Number(prompt('Zadejte teplotu ve stupních Fahrenheita:'))
 
 document.body.innerHTML = '<h1>' + 'Převodník stupňů Fahrenheita na stupně Celsia' + '</h1>'
-document.body.innerHTML += '<p>' + 'Teplota ve stupních Celsia: ' + 5 * (stupneFahrenheita - 32) / 9 + ' °C ' + '</p>'
+document.body.innerHTML += '<p>' + 'Teplota ve stupních Celsia: ' + 5 * (stupneFahrenheita - 32) / 9 + ' °C' + '</p>'
